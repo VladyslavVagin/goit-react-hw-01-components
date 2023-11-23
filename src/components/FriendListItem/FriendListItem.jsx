543dc17721ed24c7b308b379d97daee073c9d779
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-23 22:19:07 +0100

    update

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import css from './FriendListItem.module.css';
+import clsx from 'clsx';
 
 const FriendListItem = friend => {
     const {avatar, name, id, isOnline} = friend;
   return (
     <li className={css.item} key={id}>
-      <span className={css.status}>{isOnline}</span>
+      <span className={clsx(css.status, isOnline === true ? css.online : css.offline)}>{isOnline}</span>
       <img className={css.avatar} src={avatar} alt="{name} avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
